97e447b1e6ccf0fb2be50ff48389c7caac58923a
Tasbiha <tfb9946@nyu.edu>
2021-09-29 16:51:20 -0400

    initial commit

--- sample_time_app/run.py
+++ sample_time_app/run.py
@@ -1,10 +1,20 @@
 from flask import Flask
+
+from datetime import datetime,timedelta
+
+import time
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
     return 'Hello world!'
+
+
+@app.route('/time')
+def timer():
+    today = datetime.now() - timedelta(hours=4, minutes=0)
+    return today.strftime("%H:%M:%S")
 
 
 app.run(host='0.0.0.0',
